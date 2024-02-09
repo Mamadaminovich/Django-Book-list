212d104a0068fc585649e6853b07c8c2d790360f
Mamadaminovich <mamadaminov001@gmail.com>
2024-02-09 13:17:27 +0300

    book

---
+++ bookapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [    
+    path('book-list', bookList, name='book-list'),
+    path('book-create', bookCreate, name='book-create'),
+    path('book-update/<int:id>', bookUpdate, name='book-update'),
+    path('book-delete/<int:id>', bookDelete, name='book-delete'),
+]
